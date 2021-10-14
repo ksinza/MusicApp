ef1856d4ee8faf80509462af0e8c56ce67734a30
Kevin Sinza <ksinza@squareballstudios.com>
2021-10-14 17:22:39 -0500

    working viewmodel landingView model

---
+++ MusicAppXamarin/MusicAppXamarin/Model/Music.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MusicAppXamarin.Model
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Url { get; set; }
+        public string CoverImage { get; set; } = "https://usercontent2.hubstatic.com/14548043_f1024.jpg";
+        public bool IsRecent { get; set; }
+
+    }
+}
--- MusicAppXamarin/MusicAppXamarin/ViewModel/BaseViewModel.cs
+++ MusicAppXamarin/MusicAppXamarin/ViewModel/BaseViewModel.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace MusicAppXamarin.ViewModel
 {
     public class BaseViewModel
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
         public BaseViewModel()
         {
         }
